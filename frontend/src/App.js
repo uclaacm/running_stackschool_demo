ee2924e4096912c03275f55845199002e4d0767d
EinarBalan <einar.balan@gmail.com>
2023-02-16 19:42:05 -0800

    workshop 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  let [posts, setPosts] = useState([]); 
+  const [message, setMessage] = useState('');
+  const URL = "http://localhost:8080"
+
+  const getFeed = () => {
+    axios.get(URL + "/feed")
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(console.error)
+  }
+
+  useEffect(() => {
+    getFeed();
+  }, [posts]);
+
+  function addPost () {
+
+    if(message === '') {
+      alert('message cannot be empty!');
+      return;
+    }
+
+    axios.post(URL + '/feed/new', {
+      content: message,
+      user: "testuser"
+    }).then(response => console.log(response))
+    .catch(console.error)
+
+    setMessage('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input
+          type='text'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder='message here!!!'
+        />
+        <button onClick={() => addPost()}> Post </button>
+      </div>
+      {posts.map((post, i) => 
+        <div key={i}>
+          <h3>{post.user}</h3>
+          <p>{post.content}</p>
+          <p>{post.num_likes} {post.timestamp}</p>
+        </div>  
+      )}
     </div>
   );
 }
